1a56e1d4f2040fe5d0a35f55b7de506897fa8e08
George Z. Zachos <gzachos@cse.uoi.gr>
2021-08-30 11:51:08 +0300

    Added Figures/{parade,paragon}-epcc

---
+++ Figures/parade-epcc/fetch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cp ~/git/thesis-testspace/GRAPHS/epcc_20210829_035042/{ompionly_,}sockets_tpn-8_close.pdf  .
+cp ~/git/thesis-testspace/GRAPHS/epcc_20210829_031754/{ompionly_,}sockets_tpn-16_close.pdf .
+cp ~/git/thesis-testspace/GRAPHS/epcc_20210829_222355/{ompionly_,}sockets_tpn-32_close.pdf .
+
+cp ~/git/thesis-testspace/GRAPHS/epcc_20210829_015651/{ompionly_,}topocores_tpn-16_close.pdf .
+
+cp ~/git/thesis-testspace/GRAPHS/epcc_20210829_222057/{ompionly_,}topothreads_tpn-8_spread.pdf .
+cp ~/git/thesis-testspace/GRAPHS/epcc_20210829_222057/{ompionly_,}topothreads_tpn-16_spread.pdf .
+cp ~/git/thesis-testspace/GRAPHS/epcc_20210829_031754/{ompionly_,}topothreads_tpn-32_close.pdf .
+
